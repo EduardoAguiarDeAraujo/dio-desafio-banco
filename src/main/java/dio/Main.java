fec02c1eaf2a888347d9c78884b8f5fe6f943302
Eduardo Aguiar de Araujo <araujo.ea@gmail.com>
2024-05-16 15:03:19 -0300

    First commit

---
+++ src/main/java/dio/Main.java
@@ -0,0 +1,19 @@
+package dio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente eduardo = new Cliente();
+		eduardo.setNome("Eduardo");
+		
+		Conta cc = new ContaCorrente(eduardo);
+		Conta poupanca = new ContaPoupanca(eduardo);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
